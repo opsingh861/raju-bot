407c9686f26015d55fa3325fd1c0e06fb106061b
Aditya Dhanraj <opsingh861@gmail.com>
2024-03-02 16:02:20 +0530

    added route so can restart the server

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
+const express = require('express');
+const app = express();
+
 const dotenv = require('dotenv');
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 
 // Telegram Bot Token
 dotenv.config();
@@ -20,7 +31,7 @@ async function getOpenAIResponse(query) {
             'https://api.openai.com/v1/chat/completions',
             {
                 "model": "gpt-3.5-turbo",
-                "messages": [{"role":"system","content":"Your name is Raju and You are very friendly in nature"},{ "role": "user", "content": query }],
+                "messages": [{ "role": "system", "content": "Your name is Raju and You are very friendly in nature" }, { "role": "user", "content": query }],
                 "temperature": 0.7
             },
             {
@@ -64,4 +75,8 @@ bot.on('message', async (msg) => {
     }
     const openaiResponse = await getOpenAIResponse(userQuery);
     bot.sendMessage(chatId, openaiResponse);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
